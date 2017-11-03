9c4035e5d9228e7e8e51bd9c97368279287a004f
sasanka <sasanka@Macs-MacBook-Pro.local>
2017-11-02 22:18:25 -0700

    pyfun

---
+++ hello_world.py
@@ -0,0 +1,22 @@
+# x = "H e l l o W o r l d"
+# print x.upper()
+
+# print x.count("l")
+# print x.endswith("1")
+# print x.find("d")
+# print x.isalnum()
+
+url = "mkyong.com#100#2015-10-1"
+data = url.split("#")
+
+print data
+
+for i in data:
+    print i
+
+# print url.split("#")
+#
+# print len(data) #3
+# print data[0]  # mkyong.com
+# print data[1]  # 100
+# print data[2]  # 2015-10-1
---
+++ string_list.py
@@ -0,0 +1,12 @@
+words = "it's thanksgiving day. It's my Birthday,too"
+
+print words.find('day')
+
+new_words = words.replace("day","month")
+
+print new_words
+
+
+x = [2,54,-2,7,12,98]
+
+print max(x)
