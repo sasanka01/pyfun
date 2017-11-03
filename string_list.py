0523dfbf626d4c04991e1de135a120a06a562854
sasanka <sasanka@Macs-MacBook-Pro.local>
2017-11-02 23:19:50 -0700

    strings and list

--- string_list.py
+++ string_list.py
@@ -10,3 +10,24 @@ print new_words
 x = [2,54,-2,7,12,98]
 
 print max(x)
+print min(x)
+
+y = ["hello",2,54,-2,7,12,98,"world"]
+
+z = [y[0],y[-1]]
+print z
+
+a = [19,2,54,-2,7,12,98,32,10,-3,6]
+
+a.sort()
+print a
+
+
+b = a[:len(a)/2]
+
+print b
+
+del a[:5]
+
+a.insert(0,b)
+print a
