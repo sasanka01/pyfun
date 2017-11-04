690c7dbad040ed71aa5c551a9e07bd28c2eb0b4d
sasanka <sasanka@Macs-MacBook-Pro.local>
2017-11-04 16:02:52 -0700

    checkerboard

--- checkerboard.py
+++ checkerboard.py
@@ -1,7 +1,5 @@
 for i in range(1, 9):
-        if (i%10==0):
-            output = "\n"
-        elif(i%2==0):
+        if(i%2==0):
             output = " *" * 4
         else:
             output = "* " * 4
