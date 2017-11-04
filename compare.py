0c02eb2e53bca0f93950512ad68e07a193875120
sasanka <sasanka@Macs-MacBook-Pro.local>
2017-11-04 02:25:54 -0700

    compare

---
+++ compare.py
@@ -0,0 +1,10 @@
+# list_one = [1,2,5,6,2]
+# list_two = [1,2,5,6,2]
+
+list_one = ['celery','carrots','bread','milk']
+list_two = ['celery','carrots','bread','cream']
+
+if list_one == list_two:
+    print "they are equal"
+else:
+    print "they are different"
