e7ec816746d1ec2491f95a56732bfee02c2165dd
sasanka <sasanka@Macs-MacBook-Pro.local>
2017-11-03 23:22:53 -0700

    multiples average sum

---
+++ mulsumavg.py
@@ -0,0 +1,21 @@
+# # printing all the odd numbers from 1 to 1000
+
+for i in range (1,1000,2):
+    print i
+
+
+# printing all the multiples of 5 from 5 to 1,000,000
+
+for i in range (5,1000001,5):
+     print i
+
+
+# sum of all values in the list
+
+a = [1, 2, 5, 10, 255, 3]
+# print sum(a)
+
+
+# print the average of the list
+
+print sum(a)/len(a)
