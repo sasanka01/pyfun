7e955e3922e823e70650ab17d861bb0c719e79d9
sasanka <sasanka@Macs-MacBook-Pro.local>
2017-11-04 20:39:37 -0700

    fun with functions

--- fwf.py
+++ fwf.py
@@ -12,9 +12,20 @@ def multiply(arr,num):
     # print arr, num
     for i in range (0,len(arr)):
         arr[i] = arr[i] * num
-    print arr
+    # print arr
     return arr
 arr = [2,4,10,16]
 num = 5
 b = multiply(arr,num)
-b
+# b
+
+def layered_multiples(arr):
+    new_array = []
+    for x in arr:
+        new_array1 = []
+        for i in range (0,x):
+            new_array1.append(1)
+        new_array.append(new_array1)
+    return new_array
+x = layered_multiples(multiply([2,4,5],3)) #is equal to arr = [6,12,15]
+print x
