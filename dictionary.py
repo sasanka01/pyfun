9ac66e3a81652041adadc6970bbd0a8f9e964335
sasanka <sasanka@Macs-MacBook-Pro.local>
2017-11-05 17:41:31 -0800

    dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,8 +1,10 @@
+def dictionary_value(dic):
+    for key,value in my_var.iteritems():
+        print "My {} is {}".format(key,value)
+
 my_var = {'name':'SaSanka',
 'age' : 29,
 'country' : 'India',
 'Language' : 'Pyhton'
 }
-
-for key,value in my_var.iteritems():
-    print "My {} is {}".format(key,value)
+dictionary_value(my_var)
